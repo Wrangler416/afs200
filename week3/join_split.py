8f3b8eb258c399ed1896c6dadb59ce0a8293bdbb
kara temple <you@example.com>
2021-05-01 13:21:36 -0400

    done

---
+++ week3/join_split.py
@@ -0,0 +1,16 @@
+my_list = []
+names = ["Eric", "John", "Michael", "Terry", "Graham", "TerryG", "Brian"]
+
+
+my_list.append(names[0])
+my_list
+my_list.append(names[1])
+my_list.append(names[2])
+my_list.append(names[3])
+my_list.append(names[4])
+my_list.append(names[5])
+my_list.append(names[6])
+
+print(my_list)
+
+
