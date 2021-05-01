f4f182cd3ab8f8f4e875accb386e7c2824143ec5
kara temple <you@example.com>
2021-05-01 13:11:54 -0400

    week3

--- week2/project/oddeven.py
+++ week2/project/oddeven.py
@@ -3,7 +3,7 @@ if (number % 2) == 0:
    print("Your number is Even")
 else:
    print("Your number is odd")
-if (number % 4) ==0:
+if (number % 4) == 0:
     print("Your number is a multiple of four")
 
 
---
+++ week3/tuple.py
@@ -0,0 +1,5 @@
+Tuple1 = ("Robert","Smith", 1987, "favorite_movie and year", "IT", "Texas")
+Tuple2 = (1,2,3,4,5,6,7); 
+
+print(Tuple1[0])
+print(Tuple2[1:4] )
